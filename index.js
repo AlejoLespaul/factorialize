e218cb535131668996fd68b2eb2f44d142c517c8
afurgeri <alejandro.furgeri@sondeos.com.ar>
2021-01-21 16:38:23 -0300

    Resuelto

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const factorialize = function(num){
+    var result = num
 
-    /* YOUR CODE HERE */
+    for(var i = (num - 1) ; i > 1; i-- ){
+        result = result * i
+    }
 
+    return result
 }
 
 module.exports = factorialize
